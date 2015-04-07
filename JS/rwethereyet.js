0852848ae63615bb50a495d74545d04c79819aec
yonathanbenelli <ybenelli1234@gmail.com>
2015-04-07 16:56:51 -0300

    asd

--- JS/rwethereyet.js
+++ JS/rwethereyet.js
@@ -91,12 +91,12 @@ $('#menu1button1').attr('src','resources/buttons/tripplannerbutton_'+resolution+
 $('#menu1button2').attr('src','resources/buttons/settingsbutton_'+resolution+'.png');
 $('#menu1button3').attr('src','resources/buttons/helpbutton_'+resolution+'.png');
 $('#backMain').attr('src','resources/buttons/backarrow_'+resolution+'.png');
-$('#fish10').css('background-image','url(resources/characters/aquarium/animated/fish1_1.png)');
+//$('#fish10').css('background-image','url(resources/characters/aquarium/animated/fish1_1.png)');
 });
 
 
 $(document).on('pageshow','#main', function(e,data){    
-$('#fish10')
+/*$('#fish11')
 			.sprite({fps: 4, no_of_frames:6}).activeOnClick().active()
 			.flyToTap()
 			.spRandom({
@@ -107,7 +107,7 @@ $('#fish10')
           speed: 4000,
           pause: 3000
       });
-	  
+	*/  
 if(distanceLeft>0 || (timeLeft>0 && timeFull-timeLeft>0))
 {
 	$('#backMain').css('visibility','visible');	
