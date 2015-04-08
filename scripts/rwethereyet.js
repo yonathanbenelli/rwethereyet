7b6c618cce4a38e9f8ac4a08e1af70f6b78f4edc
yonathanbenelli <ybenelli1234@gmail.com>
2015-04-08 02:36:04 -0300

    piso2

---
+++ scripts/rwethereyet.js
@@ -0,0 +1,15 @@
+
+		(function($) {
+			$(document).ready(function() {
+			
+				$('#bird')
+					.sprite({fps: 9, no_of_frames: 3})
+					.spRandom({top: 50, bottom: 200, left: 300, right: 320})
+					.isDraggable()
+					.activeOnClick()
+					.active();
+				
+				
+			});
+		})(jQuery);
+	
